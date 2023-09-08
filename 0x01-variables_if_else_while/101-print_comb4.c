5cbe697e9e87926b2f02d1fb716b4e0fb30225c9
EL ORCHE <amineelorche@gmail.com>
2023-09-08 13:38:48 -0700

    script added

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,46 +1,43 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all possible combinations
- * followed by a new line
- * description: 'The success combination'
- * Return: always 0
+ * main - Prints all possible different combinations of three digits.
+ * Description: Digits must be separated by commas and a space.
+ * Digits must be printed in ascending order.
+ * You can only use `putchar` five times maximum in your code.
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-int digit1 = 0;
-int digit2, digit3;
+  int digit1 = 0;
+  int digit2, digit3;
 
-while (digit1 <= 9)
-{
-digit2 = 0;
-while (digit2 <= 9)
-{
-digit3 = 0;
-while (digit3 <= 9)
-{
-if (digit1 != digit2 &&
-    digit1 < digit2 &&
-    digit2 != digit3 &&
-    digit2 < digit3)
-{
-putchar(digit1 + 48);
-putchar(digit2 + 48);
-putchar(digit3 + 48);
+  while (digit1 <= 9)
+    {
+      digit2 = 0;
+      while (digit2 <= 9)
+	{
+	  digit3 = 0;
+	  while (digit3 <= 9)
+	    {
+	      if (digit1 < digit2 && digit2 < digit3)
+		{
+		  putchar(digit1 + '0');
+		  putchar(digit2 + '0');
+		  putchar(digit3 + '0');
 
-if (digit1 + digit2 + digit3 != 24)
-{
-putchar(',');
-putchar(' ');
-}
-}
-digit3++;
-}
-digit2++;
-}
-digit1++;
-}
-putchar('\n');
-return (0);
+		  if (digit1 + digit2 + digit3 != 24)
+		    {
+		      putchar(',');
+		      putchar(' ');
+		    }
+		}
+	      digit3++;
+	    }
+	  digit2++;
+	}
+      digit1++;
+    }
+  putchar('\n');
+  return (0);
 }
