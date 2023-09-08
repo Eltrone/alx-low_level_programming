89ba065ca6244d5e4aafe4d7bcd42559c110bf7b
EL ORCHE <amineelorche@gmail.com>
2023-09-08 13:26:08 -0700

    script added

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,22 +8,23 @@
  */
 int main(void)
 {
-0-positive_or_negative.c int n;
+  int n;
 
-0-positive_or_negative.c srand(time(0));
-0-positive_or_negative.c n = rand() - RAND_MAX / 2;
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
 
-0-positive_or_negative.c if ((n % 10) > 5)
-0-positive_or_negative.c {
-0-positive_or_negative.c 0-positive_or_negative.c printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-0-positive_or_negative.c }
-0-positive_or_negative.c else if ((n % 10) < 6 && (n % 10) != 0)
-0-positive_or_negative.c {
-0-positive_or_negative.c 0-positive_or_negative.c printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-0-positive_or_negative.c }
-0-positive_or_negative.c else if ((n & 10) == 0)
-0-positive_or_negative.c {
-0-positive_or_negative.c 0-positive_or_negative.c printf("Last digit of %d is %d and is 0\n", n, n % 10);
-0-positive_or_negative.c }
-0-positive_or_negative.c return (0);
+  if ((n % 10) > 5)
+    {
+      printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+    }
+  else if ((n % 10) < 6 && (n % 10) != 0)
+    {
+      printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+    }
+  else if ((n & 10) == 0)
+    {
+      printf("Last digit of %d is %d and is 0\n", n, n % 10);
+    }
+  return (0);
 }
+
