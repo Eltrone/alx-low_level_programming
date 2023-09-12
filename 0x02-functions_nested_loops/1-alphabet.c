2d7d01722f8263873b083921fa867d368118608f
EL ORCHE <amineelorche@gmail.com>
2023-09-12 10:56:02 -0700

    script added

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,15 +6,12 @@
  * Return: no return.
  */
 
-#include "main.h"
-
 void print_alphabet(void)
 {
   char c;
-
   for (c = 'a'; c <= 'z'; c++)
-    {
+  {
       _putchar(c);
-    }
+  }
   _putchar('\n');
 }
