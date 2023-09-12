3b4d10a68fb39e6be7e62870afe3b8a1c655b768
EL ORCHE <amineelorche@gmail.com>
2023-09-12 13:51:38 -0700

    script added

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet - a function that prints the alphabet,
- * in lowercase, followed by a new line
- * Return: no return.
- */
+ *  * print_alphabet - a function that prints the alphabet,
+ *   * in lowercase, followed by a new line
+ *    * Return: no return.
+ *     */
+
 void print_alphabet(void)
 {
-int i = 97;
+		int i = 97;
 
-for (i = 97 ; i < 123 ; i++)
-{
-_putchar(i);
+			for (i = 97 ; i < 123 ; i++)
+					{
+								_putchar(i);
+									}
+				_putchar('\n');
 }
-_putchar(n);
-}
+
