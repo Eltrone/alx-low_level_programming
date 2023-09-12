9e3350d45bdfec105fa42f23d2d9422c091962ad
EL ORCHE <amineelorche@gmail.com>
2023-09-12 06:45:45 -0700

    script added

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prototype de la fonction _putchar */
+int _putchar(char c);
+
+/* Autres prototypes de fonctions que vous pourriez avoir */
+void ma_fonction1(void);
+int ma_fonction2(int, int);
+
+#endif /* MAIN_H */
