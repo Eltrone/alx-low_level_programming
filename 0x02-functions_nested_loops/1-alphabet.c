5cb5c583a1ca0cf35202298252d91ad4340e7b20
EL ORCHE <amineelorche@gmail.com>
2023-09-12 10:39:40 -0700

    script added

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,14 @@
  * in lowercase, followed by a new line
  * Return: no return.
  */
+
 void print_alphabet(void)
 {
-0-putchar.c int i = 97;
+  int i = 97;
 
-0-putchar.c for (i = 97 ; i < 123 ; i++)
-0-putchar.c {
-0-putchar.c 0-putchar.c _putchar(i);
-0-putchar.c }
-0-putchar.c _putchar('\n');
+  for (i = 97 ; i < 123 ; i++)
+    {
+      _putchar(i);
+    }
+  _putchar('\n');
 }
