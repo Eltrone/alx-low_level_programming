b18cdaa4adcbec848e9c5c17796593da25a9468e
EL ORCHE <amineelorche@gmail.com>
2023-09-18 12:26:32 -0700

    program modified

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,27 +3,23 @@
 /**
 * print_rev - Prints a string in reverse followed by a new line.
 * @s: The string to be printed in reverse.
-*
-* Description: Iterates to the end of the string and then
-* prints each character in reverse order.
-* Return: None.
 */
-
 void print_rev(char *s)
 {
-    int length = 0;
-    int i;
+	int length = 0;
+	int i;
 
-    /* Find the end of the string */
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	/* Find the end of the string */
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-    /* Print the string in reverse */
-    for (i = length - 1; i >= 0; i--)
-    {
-        _putchar(s[i]);
-    }
-    _putchar('\n');
+	/* Print the string in reverse */
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
+
