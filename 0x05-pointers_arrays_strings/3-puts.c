eb77deb1300f71e96bc607e3ceab560dd16dd414
EL ORCHE <amineelorche@gmail.com>
2023-09-18 12:12:54 -0700

    program modified

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: The string to be printed.
+ *
+ * Description: Iterates through the string and prints each character.
+ * Return: None.
+ */
+void _puts(char *str)
+{
+    while (*str != '\0')
+    {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n');
+}
