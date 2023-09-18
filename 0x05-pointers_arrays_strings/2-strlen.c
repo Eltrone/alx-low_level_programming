bfd99c32fb82c884ce3b14725b9040c40d7fc24d
EL ORCHE <amineelorche@gmail.com>
2023-09-18 12:06:37 -0700

    program modified

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @s: The string to get the length of.
- *
- * Description: Iterates through the string until it reaches
- * the null terminator and counts the number of characters.
- * Return: The length of the string.
- */
+* _strlen - Returns the length of a string.
+* @s: The string to get the length of.
+*
+* Description: Iterates through the string until it reaches
+* the null terminator and counts the number of characters.
+* Return: The length of the string.
+*/
 int _strlen(char *s)
 {
     int length = 0;
