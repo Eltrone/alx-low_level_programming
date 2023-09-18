659a4265c649238a47a1bb3c3825314d76c603e7
EL ORCHE <amineelorche@gmail.com>
2023-09-18 14:38:58 -0700

    program modified

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+100-atoi.c
+#include "main.h"
+
+/**
+* _atoi - Converts a string to an integer.
+* @s: Input string.
+* Return: The converted integer.
+*/
+
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int line = 1;
+
+	do {
+		if (*s == '-')
+			line *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * line);
+}
