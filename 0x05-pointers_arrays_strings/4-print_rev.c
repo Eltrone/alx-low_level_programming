7cfe86d72e27d8b64a42b409ee2ed8c70d669ef7
EL ORCHE <amineelorche@gmail.com>
2023-09-18 12:22:24 -0700

    program modified

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,13 +13,13 @@ void print_rev(char *s)
 {
     int length = 0;
 
-    // Find the end of the string
+    /* Find the end of the string */
     while (s[length] != '\0')
     {
         length++;
     }
 
-    // Print the string in reverse
+    /* Print the string in reverse */
     for (int i = length - 1; i >= 0; i--)
     {
         _putchar(s[i]);
