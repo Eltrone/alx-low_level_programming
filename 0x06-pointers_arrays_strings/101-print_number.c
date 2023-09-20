cda3441aa1776c202dac31105549bc8770ef3820
EL ORCHE <amineelorche@gmail.com>
2023-09-20 16:23:52 -0700

    .c file added

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
 /**
-* print_number - Prints an integer
-* @n: The integer to be printed
-*
-* Return: void
-*/
+ * print_number -  a function that prints an integer.
+ *
+ * @n: integer params
+ *
+ * Return: 0
+ */
+
 void print_number(int n)
 {
+	unsigned int n1;
+
+	n1 = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n1 = -n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
-
-	_putchar((n % 10) + '0');
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
