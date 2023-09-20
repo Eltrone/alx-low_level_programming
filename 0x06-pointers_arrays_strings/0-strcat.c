5f353489c980f412cf7fc1befa337084f4883dfe
EL ORCHE <amineelorche@gmail.com>
2023-09-20 09:02:06 -0700

    .c file added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* _strcat - Concatenates two strings
-* @dest: The destination string
-* @src: The source string to append to dest
+*_strcat - Concatenates two strings
+*@dest: The destination string
+*@src: The source string to append to dest
 *
-* Return: A pointer to the resulting string dest
+*Return: A pointer to the resulting string dest
 */
 char *_strcat(char *dest, char *src)
 {
