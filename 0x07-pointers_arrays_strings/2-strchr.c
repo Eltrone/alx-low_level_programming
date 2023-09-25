3dd91a785c38896783ab875034609502d1b805d1
EL ORCHE <amineelorche@gmail.com>
2023-09-25 15:34:56 -0700

    script added

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strchr - locates a character in a string.
+* @s: string to search in
+* @c: character to search for
+* Return: pointer to the first occurrence of the character c in the string s,
+* or NULL if the character is not found
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	// Check for the null character
+	if (c == '\0')
+	{
+		return (s);
+	}
+
+	return (NULL);
+}
