f80b8898d27c4ee4e0ee676687240eb5ba2e0081
EL ORCHE <amineelorche@gmail.com>
2023-10-07 08:38:32 -0700

    script added

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,87 +1,102 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#define ERR_MSG "Error"
 
 /**
-* is_digit - checks if a string contains a non-digit char
-* @s: string to be evaluated
-* Return: 0 if a non-digit is found, 1 otherwise
+* is_digit - Verifies if all characters in a string are digits
+* @s: The string to check
+*
+* Return: 1 if all characters are digits, 0 otherwise
 */
 int is_digit(char *s)
 {
 	int i = 0;
-
 	while (s[i])
-		if (s[i] < '0' || s[i++] > '9')
+	{
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
+		i++;
+	}
 	return (1);
 }
 
 /**
-* _strlen - returns the length of a string
-* @s: string to evaluate
-* Return: the length of the string
+* _strlen - Calculates the length of a given string
+* @s: The string whose length is to be calculated
+*
+* Return: The length of the string
 */
 int _strlen(char *s)
 {
 	int i = 0;
 
-	while (s[i])
+	while (s[i] != '\0')
+	{
 		i++;
+	}
 	return (i);
 }
 
 /**
-* errors - handles errors for main
+* errors - Manages error handling for the main function
 */
 void errors(void)
 {
-	_putchar('E');
-	_putchar('r');
-	_putchar('r');
-	_putchar('o');
-	_putchar('r');
-	_putchar('\n');
+	printf("Error\n");
 	exit(98);
 }
 
 /**
-* main - multiplies two positive numbers
-* @argc: number of arguments
-* @argv: array of arguments
-* Return: always 0 (Success)
+* main - Multiplies two positive integers
+* @argc: The number of command line arguments
+* @argv: The command line arguments
+*
+* Return: 0 on success
 */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])}
 {
-	char *num1, *num2;
-	int len1, len2, len, i, j, carry, *result, flag = 0;
-
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-		errors();
-	num1 = argv[1], num2 = argv[2];
-	len1 = _strlen(num1), len2 = _strlen(num2);
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+	errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	result = malloc(sizeof(int) * len);
+
 	if (!result)
-		errors();
+		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (i = len1 - 1; i >= 0; i--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		carry = 0;
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			carry += result[i + j + 1] + ((num1[i] - '0') * (num2[j] - '0'));
-			result[i + j + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry)
-			result[i + j + 1] += carry;
+	digit1 = s1[len1] - '0';
+	carry = 0;
+	for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+	{
+	digit2 = s2[len2] - '0';
+	carry += result[len1 + len2 + 1] + (digit1 * digit2);
+	result[len1 + len2 + 1] = carry % 10;
+	carry /= 10;
 	}
+
+	if (carry > 0)
+	result[len1 + len2 + 1] += carry;
+	}
+
 	for (i = 0; i < len - 1; i++)
+	{
 		if (result[i])
-			flag = 1, _putchar(result[i] + '0');
-	if (!flag)
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+
+	if (!a)
 		_putchar('0');
+
 	_putchar('\n');
 	free(result);
 	return (0);
