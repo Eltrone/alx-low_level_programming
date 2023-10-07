0f6200b07e4b4861e880adfa81aa82c4396a2eb9
EL ORCHE <amineelorche@gmail.com>
2023-10-07 07:58:38 -0700

    script added

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,14 +3,14 @@
 
 #include <stdlib.h>
 
-/* Prototypes for previous tasks */
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-/* Prototypes for 101-mul.c */
+
 int _putchar(char c);
 int is_number(char *s);
 void multiply(char *num1, char *num2);
