2ea21e320bc77b92906046f5d0133f78f202e341
EL ORCHE <amineelorche@gmail.com>
2023-10-08 03:07:21 -0700

    script added

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,99 +1,108 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
-* is_digit - checks if a string contains a non-digit char
-* @s: string to be evaluated
-*
-* Return: 0 if a non-digit is found, 1 otherwise
+* main - Entry point
+* @argc: argument count
+* @argv: argument vector
+* @return: 0 on success, 98 otherwise
+*/
+int main(int argc, char *argv[])
+{
+	char *num1, *num2;
+
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+	{
+		errors();
+		exit(98);
+	}
+	num1 = argv[1];
+	num2 = argv[2];
+	multiply(num1, num2);
+	return (0);
+}
+
+/**
+* errors - Print an error message to stdout
+*/
+void errors(void)
+{
+	char *err = "Error";
+
+	while (*err)
+		_putchar(*err++);
+	_putchar('\n');
+}
+
+/**
+* is_digit - Check if string s consists of digits only
+* @s: string to be checked
+* @return: 1 if s consists of digits, 0 otherwise
 */
 int is_digit(char *s)
 {
-	int i = 0;
-
-	while (s[i])
+	while (*s)
 	{
-		if (s[i] < '0' || s[i] > '9')
+		if (*s < '0' || *s > '9')
 			return (0);
-		i++;
+		s++;
 	}
 	return (1);
 }
 
 /**
-* _strlen - returns the length of a string
-* @s: string to evaluate
-*
-* Return: the length of the string
+* multiply - Multiply two numbers and print the result
+* @num1: first number
+* @num2: second number
 */
-int _strlen(char *s)
+void multiply(char *num1, char *num2)
 {
-	int i = 0;
+	int len1, len2, *result, i, j, carry, temp;
 
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-/**
-* errors - handles errors for main
-*/
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
-}
-
-/**
-* main - multiplies two positive numbers
-* @argc: number of arguments
-* @argv: array of arguments
-*
-* Return: always 0 (Success)
-*/
-int main(int argc, char *argv[])
-{
-	char *s1, *s2;
-	int len1, len2, len, i, j, carry, digit1, digit2, *result, a = 0;
-
-	if (argc != 3 || !is_digit(s1 = argv[1]) || !is_digit(s2 = argv[2]))
-		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	result = (int *)malloc((len1 + len2) * sizeof(int));
 	if (!result)
-		return (1);
-
-	for (i = 0; i < len; i++)
+	{
+		errors();
+		exit(98);
+	}
+	for (i = 0; i < len1 + len2; i++)
 		result[i] = 0;
 
 	for (i = len1 - 1; i >= 0; i--)
 	{
-		digit1 = s1[i] - '0';
 		carry = 0;
 		for (j = len2 - 1; j >= 0; j--)
 		{
-			digit2 = s2[j] - '0';
-			carry += result[i + j + 1] + (digit1 * digit2);
-			result[i + j + 1] = carry % 10;
-			carry /= 10;
+			temp = (num1[i] - '0') * (num2[j] - '0') + carry + result[i + j + 1];
+			carry = temp / 10;
+			result[i + j + 1] = temp % 10;
 		}
-		if (carry > 0)
-			result[i + j + 1] += carry;
+		result[i] += carry;
 	}
-
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len1 + len2; i++)
 	{
-		if (result[i])
-			a = 1;
-		if (a)
-			putchar(result[i] + '0');
+		if (i == 0 && result[i] == 0)
+			continue;
+		_putchar(result[i] + '0');
 	}
-	if (!a)
-		putchar('0');
-	putchar('\n');
+	_putchar('\n');
 	free(result);
-	return (0);
+}
+
+/**
+* _strlen - Calculate the length of a string
+* @s: the string
+* @return: length of the string
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,5 +13,6 @@ int _putchar(char c);
 void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);
+void multiply(char *num1, char *num2);
 
 #endif /* MAIN_H */
