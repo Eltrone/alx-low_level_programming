fd1e04a80bb0c0dd6119d73c65b9dcf5ad05e643
EL ORCHE <amineelorche@gmail.com>
2023-10-08 13:48:42 -0700

    script added

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,103 +1,138 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* main - multiplies two large numbers
+* main - Entry point, multiplies two numbers
 * @argc: Argument count
 * @argv: Argument vector
-* Return: 0 on success, 98 on failure
+* Return: 0 on success, 98 on error
 */
 int main(int argc, char *argv[])
 {
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-	{
-		errors();
-		exit(98);
-	}
-	multiply(argv[1], argv[2]);
+	char *num1, *num2, *result;
+	int len1, len2, lenr;
+
+	if (argc != 3)
+		return (error_exit(NULL));
+
+	num1 = argv[1];
+	num2 = argv[2];
+	len1 = _strlen(num1);
+	len2 = _strlen(num2);
+	lenr = len1 + len2 + 1;
+	result = malloc(lenr);
+	if (!result)
+		return (error_exit(NULL));
+
+	initialize_result(result, lenr);
+	multiply_numbers(num1, num2, result, len1, len2, lenr);
+	print_result(result, lenr);
+
+	free(result);
 	return (0);
 }
 
 /**
-* errors - prints "Error" followed by a new line to stdout
+* initialize_result - Initializes the result array
+* @result: Result array
+* @lenr: Length of the result array
 */
-void errors(void)
+void initialize_result(char *result, int lenr)
 {
-	char *err = "Error";
+	int i;
 
-	while (*err)
-		_putchar(*err++);
-	_putchar('\n');
+	for (i = 0; i < lenr; i++)
+		result[i] = '0';
+	result[i] = '\0';
 }
 
 /**
-* is_digit - Checks if a string only contains digits
-* @s: String to check
-* Return: 1 if all digits, 0 otherwise
+* multiply_numbers - Multiplies the numbers and stores in result
+* @num1: First number
+* @num2: Second number
+* @result: Result array
+* @len1: Length of num1
+* @len2: Length of num2
+* @lenr: Length of result
 */
-int is_digit(char *s)
+void multiply_numbers(char *num1, char *num2, char *result, int len1, int len2, int lenr)
 {
-	while (*s)
-	{
-		if (*s < '0' || *s > '9')
-			return (0);
-		s++;
-	}
-	return (1);
-}
+	int i, j, carry, tmp;
 
-/**
-* multiply - Multiplies two large numbers
-* @num1: first number
-* @num2: second number
-*/
-void multiply(char *num1, char *num2)
-{
-	int len1, len2, *result, i, j, carry, temp;
-
-	len1 = _strlen(num1);
-	len2 = _strlen(num2);
-	result = (int *)malloc((len1 + len2) * sizeof(int));
-	if (!result)
-	{
-		errors();
-		exit(98);
-	}
-	for (i = 0; i < len1 + len2; i++)
-		result[i] = 0;
 	for (i = len1 - 1; i >= 0; i--)
 	{
+		if (num1[i] < '0' || num1[i] > '9')
+			error_exit(result);
 		carry = 0;
 		for (j = len2 - 1; j >= 0; j--)
 		{
-			temp = (num1[i] - '0') * (num2[j] - '0') + carry + result[i + j + 1];
-			carry = temp / 10;
-			result[i + j + 1] = temp % 10;
+			if (num2[j] < '0' || num2[j] > '9')
+				error_exit(result);
+			tmp = (num1[i] - '0') * (num2[j] - '0') + carry + (result[i + j + 1] - '0');
+			carry = tmp / 10;
+			result[i + j + 1] = (tmp % 10) + '0';
 		}
 		result[i] += carry;
 	}
-	for (i = 0; i < len1 + len2; i++)
-	{
-		if (i == 0 && result[i] == 0)
-			continue;
-		_putchar(result[i] + '0');
-	}
-	_putchar('\n');
-	free(result);
 }
 
 /**
-* _strlen - Calculates the length of a string
-* @s: String to calculate the length of
-* Return: Length of string
+* print_result - Prints the result array
+* @result: Result array
+* @lenr: Length of result array
+*/
+void print_result(char *result, int lenr)
+{
+	int i;
+
+	for (i = 0; result[i] == '0' && i < lenr - 1; i++)
+		;
+	_putchar(result + i);
+}
+
+/**
+* _strlen - returns the length of a string
+* @s: string
+* Return: length of string
 */
 int _strlen(char *s)
 {
-	int len = 0;
+	int i = 0;
 
-	while (*s)
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+* _putchar - writes the string to stdout
+* @str: The string to print
+*/
+void _putchar(char *str)
+{
+	int i = 0;
+
+	while (str[i])
 	{
-		len++;
-		s++;
+		write(1, &str[i], 1);
+		i++;
 	}
-	return (len);
+	write(1, "\n", 1);
+}
+
+/**
+* error_exit - exits with status 98 and prints Error
+* @result: pointer to free before exiting
+* Return: 98
+*/
+int error_exit(char *result)
+{
+	free(result);
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
+	exit(98);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,10 +9,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _putchar(char c);
-void errors(void);
 int _strlen(char *s);
-int is_digit(char *s);
-void multiply(char *num1, char *num2);
+void _putchar(char *str);
+int error_exit(char *result);
+void initialize_result(char *result, int lenr);
+void multiply_numbers(char *num1, char *num2, char *result, int len1, int len2, int lenr);
+void print_result(char *result, int lenr);
 
 #endif /* MAIN_H */
