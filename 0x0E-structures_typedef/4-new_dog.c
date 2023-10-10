f49d5dfbffc411d88029be84df4186809914caa2
EL ORCHE <amineelorche@gmail.com>
2023-10-10 15:11:56 -0700

    Task code added

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,14 +1,36 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+/**
+* _strdup - manually duplicates a string
+* @str: the string to duplicate
+* Return: pointer to new string, or NULL if it fails
+*/
+char *_strdup(char *str)
+{
+	int i = 0, len = 0;
+	char *newStr;
+
+	while (str[len])
+		len++;
+
+	newStr = malloc(len + 1);
+	if (newStr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		newStr[i] = str[i];
+	newStr[i] = '\0';
+
+	return (newStr);
+}
 
 /**
 * new_dog - Creates a new dog
 * @name: Name of the dog
 * @age: Age of the dog
 * @owner: Owner of the dog
-*
 * Return: Pointer to new dog_t type, or NULL if it fails
 */
 dog_t *new_dog(char *name, float age, char *owner)
@@ -20,14 +42,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (newDog == NULL)
 		return (NULL);
 
-	newName = strdup(name);
+	newName = _strdup(name);
 	if (newName == NULL)
 	{
 		free(newDog);
 		return (NULL);
 	}
 
-	newOwner = strdup(owner);
+	newOwner = _strdup(owner);
 	if (newOwner == NULL)
 	{
 		free(newName);
