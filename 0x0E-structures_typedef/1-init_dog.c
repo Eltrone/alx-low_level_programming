b798cad020077de51ed0ceb94b58b48116218b6e
EL ORCHE <amineelorche@gmail.com>
2023-10-10 14:53:57 -0700

    Task code added

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,18 +2,13 @@
 
 /**
 * init_dog - Initializes a variable of type struct dog
-* @d: The dog to initialize
+* @d: The dog struct to initialize
 * @name: The name of the dog
 * @age: The age of the dog
 * @owner: The owner of the dog
-*
-* Return: void
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
