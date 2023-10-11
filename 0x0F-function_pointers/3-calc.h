847c74f3b945ec9da24256f282c8a7f7c82cfb34
EL ORCHE <amineelorche@gmail.com>
2023-10-11 15:22:36 -0700

    Task code added

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -3,6 +3,7 @@
 
 /**
 * struct op - Struct op
+*
 * @op: The operator
 * @f: The function associated
 */
@@ -12,12 +13,15 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+/* Prototype for the function to get the correct function pointer */
+int (*get_op_func(char *s))(int, int);
+
+/* Prototypes for the operation functions */
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 
-int (*get_op_func(char *s))(int, int);
-
 #endif /* CALC_H */
+
