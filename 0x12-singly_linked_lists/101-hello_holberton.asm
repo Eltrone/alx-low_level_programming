8f72ce90e124dbcb7db2a644d7689c1ad777b3b1
EL ORCHE <amineelorche@gmail.com>
2023-10-19 14:11:09 -0700

    script added

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,18 @@
 section .data
-    hello db 'Hello, Holberton',0x0A  ; Null-terminated string to print
-    hello_len equ $ - hello           ; Calculate the length of the string
+    hello db 'Hello, Holberton',0
 
 section .text
-    global _start                     ; Entry point for the program
+    extern  printf
+    global _start
 
 _start:
-    ; sys_write (ssize_t write(int fd, const void *buf, size_t count))
-    mov rax, 1                        ; syscall number for sys_write
-    mov rdi, 1                        ; file descriptor stdout
-    mov rsi, hello                    ; pointer to the string to print
-    mov rdx, hello_len                ; length of the string to print
-    syscall                           ; invoke syscall
+    ; prepare arguments for printf
+    mov rdi, hello    ; pointer to format string
+    xor rax, rax      ; no floating point arguments
+    call printf
 
-    ; sys_exit (void _exit(int status))
-    mov rax, 60                       ; syscall number for sys_exit
-    xor rdi, rdi                      ; status 0
-    syscall                           ; invoke syscall
+    ; exit syscall
+    mov rax, 60       ; syscall number for exit
+    xor rdi, rdi      ; status 0
+    syscall
 
