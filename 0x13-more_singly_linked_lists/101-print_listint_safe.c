35ebc7d8bb79067313d1def9d7cce31b30df3320
EL ORCHE <amineelorche@gmail.com>
2023-10-23 16:32:16 -0700

    script added

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,38 +1,34 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
-* print_listint_safe - prints a listint_t linked list.
-* @head: pointer to the start of the list.
-* Return: number of nodes in the list.
-*/
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the head of the list
+ *
+ * Return: the number of nodes in the list
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow = head, *fast = head;
+	const listint_t *slow_ptr, *fast_ptr;
 	size_t count = 0;
 
 	if (!head)
 		return (0);
 
-	while (slow && fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
+	slow_ptr = head;
+	fast_ptr = head->next;
 
-		if (slow == fast)
+	while (fast_ptr && fast_ptr->next)
+	{
+		if (slow_ptr == fast_ptr)
 		{
-			do {
-				printf("[%p] %d\n", (void *)slow, slow->n);
-				count++;
-				slow = slow->next;
-			} while (slow != fast);
+			printf("-> [%p] %d\n", (void *)slow_ptr, slow_ptr->n);
 			return (count);
 		}
-	}
-	while (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next->next;
+		printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
 		count++;
-		head = head->next;
 	}
 	return (count);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,41 +2,26 @@
 
 /**
 * free_listint_safe - frees a listint_t list.
-* @h: double pointer to the start of the list.
-* Return: the size of the list that was free’d.
+* @h: double pointer to the head of the list
+*
+* Return: the size of the list that was free’d
 */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *slow = *h, *fast = *h, *temp;
 	size_t count = 0;
+	listint_t *current;
+	listint_t *next_node;
 
 	if (!h || !*h)
 		return (0);
 
-	while (slow && fast && fast->next)
+	current = *h;
+	while (current)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
-		{
-			do {
-				temp = slow;
-				slow = slow->next;
-				free(temp);
-				count++;
-			} while (slow != fast);
-			*h = NULL;
-			return (count);
-		}
-	}
-
-	while (*h)
-	{
-		temp = *h;
-		*h = (*h)->next;
-		free(temp);
 		count++;
+		next_node = current->next;
+		free(current);
+		current = next_node;
 	}
 	*h = NULL;
 	return (count);
