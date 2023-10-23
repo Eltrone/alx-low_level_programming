d0de595aa297c700065b9683729f0fd2c087b493
EL ORCHE <amineelorche@gmail.com>
2023-10-23 16:42:44 -0700

    script added

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,28 +2,34 @@
 #include <stdlib.h>
 
 /**
-* free_listint_safe - free a listint_t list safely
-* @h: double pointer to the head of the list
-*
-* Return: the size of the list that was freed
-*/
+ * free_listint_safe - free a listint_t list safely
+ * @h: double pointer to the head of the list
+ *
+ * Return: the size of the list that was freed
+ */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *temp;
-	size_t count = 0;
+	listint_t *temp, *address_array[1024];
+	size_t count = 0, i;
 
 	if (!h || !*h)
 		return (0);
 
 	while (*h)
 	{
-		count++;
+		for (i = 0; i < count; i++)
+		{
+			if (address_array[i] == *h)
+			{
+				*h = NULL;
+				return (count);
+			}
+		}
+
+		address_array[count++] = *h;
 		temp = *h;
 		*h = (*h)->next;
 		free(temp);
-
-		if (temp <= *h)
-			break;
 	}
 
 	*h = NULL;
