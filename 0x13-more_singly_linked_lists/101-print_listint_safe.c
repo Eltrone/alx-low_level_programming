e9c73db5d4ea82b7ac80438dce78d649dd04e3ed
EL ORCHE <amineelorche@gmail.com>
2023-10-23 17:23:57 -0700

    script added

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,66 +3,67 @@
 #include <stdio.h>
 
 /**
-* print_listint_safe - print a listint_t list safely
-* @head: head of the list
-*
-* Return: the number of nodes in the list
-*/
+ * print_listint_safe - print a listint_t list safely
+ * @head: head of the list
+ *
+ * Return: the number of nodes in the list
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t count = 0;
-    const listint_t *hare, *tortoise;
-    int loop_detected = 0;
+	size_t count = 0;
+	const listint_t *hare, *tortoise;
+	int loop_detected = 0;
 
-    if (head == NULL)
-        exit(98);
+	if (head == NULL)
+		exit(98);
 
-    tortoise = hare = head;
+	tortoise = hare = head;
 
-    while (hare && hare->next)
-    {
-        tortoise = tortoise->next;
-        hare = hare->next->next;
+	while (hare && hare->next)
+	{
+		tortoise = tortoise->next;
+		hare = hare->next->next;
 
-        if (tortoise == hare)
-        {
-            loop_detected = 1;
-            break;
-        }
-    }
+		if (tortoise == hare)
+		{
+			loop_detected = 1;
+			break;
+		}
+	}
 
-    if (!loop_detected)
-    {
-        while (head)
-        {
-            printf("[%p] %d\n", (void *)head, head->n);
-            count++;
-            head = head->next;
-        }
-        return (count);
-    }
+	if (!loop_detected)
+	{
+		while (head)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			count++;
+			head = head->next;
+		}
+		return (count);
+	}
 
-    tortoise = head;
-    while (tortoise != hare)
-    {
-        printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-        count++;
-        tortoise = tortoise->next;
-        hare = hare->next;
-    }
+	tortoise = head;
+	while (tortoise != hare)
+	{
+		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+		count++;
+		tortoise = tortoise->next;
+		hare = hare->next;
+	}
 
-    printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-    count++;
+	printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+	count++;
 
-    tortoise = tortoise->next;
-    while (tortoise != hare)
-    {
-        printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-        count++;
-        tortoise = tortoise->next;
-    }
+	tortoise = tortoise->next;
+	while (tortoise != hare)
+	{
+		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+		count++;
+		tortoise = tortoise->next;
+	}
 
-    printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+	printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
 
-    return (count);
+	return (count);
 }
+
