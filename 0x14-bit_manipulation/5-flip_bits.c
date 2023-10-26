385a024c97d1ce35b2b0b144f930d5010f781329
EL ORCHE <amineelorche@gmail.com>
2023-10-26 14:46:32 -0700

    script added

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits needed to flip from one number to another
- * @n: first number
- * @m: second number
- * Return: number of bits needed to flip
- */
+* flip_bits - returns the number of bits needed to flip 
+* from one number to another
+* @n: first number
+* @m: second number
+* Return: number of bits needed to flip
+*/
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int x = n ^ m;
