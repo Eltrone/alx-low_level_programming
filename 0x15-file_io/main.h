d4c03dcfed17c12793c89ed96adfa51e57135f59
EL ORCHE <amineelorche@gmail.com>
2023-10-30 15:08:13 -0700

    script added

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,7 @@ int append_text_to_file(const char *filename, char *text_content);
 void do_close(int fd);
 void display_magic(unsigned char *e_ident);
 void validate_elf(unsigned char *e_ident);
+int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
 
