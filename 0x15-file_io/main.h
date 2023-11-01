3ab001f60b358083a44990f28cc45939bd01081c
EL ORCHE <amineelorche@gmail.com>
2023-11-01 12:38:39 -0700

    script added

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Include necessary header files */
+#include <elf.h>
+#include <stdint.h>
+
 #include <unistd.h>
 #include <sys/types.h>
 
+/* Function prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
@@ -16,5 +21,6 @@ void print_header(Elf64_Ehdr header);
 const char *get_e_type(uint16_t e_type);
 const char *get_ei_osabi(unsigned char ei_osabi);
 void print_header_32(Elf32_Ehdr header);
-#endif
+
+#endif /* MAIN_H */
 
