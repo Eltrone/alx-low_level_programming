75c713db118d3a94f5d4cb8cf7514d9c8167d4ae
Eltrone <amineelorche@gmail.com>
2023-11-28 12:48:32 -0800

    script python added

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print("{}".format("".join(chr(i).upper() if i % 2 == 0 else chr(i) for i in range(122, 96, -1))), end="")
+toggle_case = 0
+for ascii_val in range(ord('z'), ord('a') - 1, -1):
+    char_to_print = chr(ascii_val - toggle_case)
+    print("{}".format(char_to_print), end="")
+    toggle_case = 0 if toggle_case else 32
