1aa586540dd7f3630faff0558d20196ce35cb341
Eltrone <amineelorche@gmail.com>
2023-11-28 12:39:11 -0800

    script python added

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    print("{}".format(chr(i)) if i % 2 != 0 else chr(i - 32), end="")
+    if i % 2 == 0:
+        print(chr(i - 32), end="")
+    else:
+        print(chr(i), end="")
