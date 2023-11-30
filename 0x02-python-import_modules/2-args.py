2ff516faf200c5ff4f812ebca0264a32864520c1
Eltrone <amineelorche@gmail.com>
2023-11-30 04:11:26 -0800

    script 2 added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,7 @@ def print_args():
     elif num_args == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(num_args))
+        print("{} arguments:".format(argc))
 
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
