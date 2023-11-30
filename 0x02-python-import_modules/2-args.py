389ca44a40ea82729c8507dedbaf7c1dcb1a3aef
Eltrone <amineelorche@gmail.com>
2023-11-30 04:15:32 -0800

    script 2 added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,16 @@
 import sys
 
 def print_args():
-    num_args = len(sys.argv) - 1
-    if num_args == 0:
+    argc = len(sys.argv) - 1  # Définition de argc dans la fonction
+
+    if argc == 0:
         print("0 arguments.")
-    elif num_args == 1:
+    elif argc == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(argc))
 
-    for i in range(1, len(sys.argv)):
+    for i in range(1, argc + 1):
         print("{}: {}".format(i, sys.argv[i]))
 
 if __name__ == "__main__":
