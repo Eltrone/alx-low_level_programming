fc2a0994fe21aec5c0c9e06fa5b1d4bc0740899e
Eltrone <amineelorche@gmail.com>
2023-12-06 13:55:27 -0800

    script added

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,13 +3,13 @@ def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
 
-
+# Exemple d'utilisation
 if __name__ == "__main__":
     a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
     new_dict = update_dictionary(a_dictionary, 'language', "Python")
 
-    import __import__('6-print_sorted_dictionary') as p
-    print_sorted_dictionary = p.print_sorted_dictionary
+    print_sorted_dictionary = __import__(
+        '6-print_sorted_dictionary').print_sorted_dictionary
     print_sorted_dictionary(new_dict)
     print("--")
     print_sorted_dictionary(a_dictionary)
