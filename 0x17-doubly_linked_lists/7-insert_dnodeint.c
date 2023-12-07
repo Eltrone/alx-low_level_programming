2ee3fb6d29e61604ec4d24e6c006e5c0a1dc1d97
Eltrone <amineelorche@gmail.com>
2023-12-07 15:04:14 -0800

    script added

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,46 +10,46 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new_node, *current;
-    unsigned int i = 0;
+	unsigned int i = 0;
+	dlistint_t *copy_head = *h;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
-    if (h == NULL)
-        return (NULL);
+	if (new == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
 
-    new_node->n = n;
-    new_node->next = NULL;
-    new_node->prev = NULL;
+	if (*h == NULL)
+	{
+		*h = new;
+		return (new);
+	}
 
-    if (idx == 0)
-    {
-        new_node->next = *h;
-        if (*h != NULL)
-            (*h)->prev = new_node;
-        *h = new_node;
-        return (new_node);
-    }
+	if (idx == 0)
+	{
+		new->next = copy_head;
+		copy_head->prev = new;
+		*h = new;
+		return (new);
+	}
 
-    current = *h;
-    while (current != NULL && i < idx)
-    {
-        if (i == idx - 1)
-        {
-            new_node->next = current->next;
-            new_node->prev = current;
-            if (current->next != NULL)
-                current->next->prev = new_node;
-            current->next = new_node;
-            return (new_node);
-        }
-        current = current->next;
-        i++;
-    }
+	while (copy_head)
+	{
+		if (i == idx - 1)
+		{
+			if (copy_head->next)
+				copy_head->next->prev = new;
+			new->next = copy_head->next;
+			copy_head->next = new;
+			new->prev = copy_head;
+			return (new);
+		}
+		copy_head = copy_head->next;
+		i++;
+	}
 
-    free(new_node);
-    return (NULL);
+	free(new);
+	return (NULL);
 }
-
