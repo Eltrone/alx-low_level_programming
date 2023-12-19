55e1b688dec89ebf30e00e17fc46a949d66e4afd
Eltrone <amineelorche@gmail.com>
2023-12-19 15:32:29 -0800

    script added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Square:
+    """Class Square that defines a square by its size.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the square to be initialized.
+        """
+        self.__size = size
