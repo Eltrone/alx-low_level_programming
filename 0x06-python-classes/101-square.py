bffb98ed125dcee123ec0f344c437dd7e1daff64
Eltrone <amineelorche@gmail.com>
2023-12-19 16:01:46 -0800

    script added

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -52,7 +52,7 @@ class Square:
         Raises:
             TypeError: If the position is not a tuple of 2 positive integers.
         """
-        if not (isinstance(value, tuple) and len(value) == 2 and 
+        if not (isinstance(value, tuple) and len(value) == 2 and
                 all(isinstance(num, int) and num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
