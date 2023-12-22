b4cf9c14059ed5f5c93d63f45164107edb725af0
Eltrone <amineelorche@gmail.com>
2023-12-22 10:57:23 -0800

    file added

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -21,11 +21,51 @@ shash_table_t *shash_table_create(unsigned long int size) {
 }
 
 int shash_table_set(shash_table_t *ht, const char *key, const char *value) {
+    shash_node_t *node, *new_node, *temp;
+    unsigned long int index;
+
     if (!ht || !key || strlen(key) == 0 || !value)
         return 0;
 
-    // ... (Ajouter la logique pour insérer le nœud dans la table et dans la liste triée)
-    return 1; // Placeholder, doit être ajusté selon la logique d'insertion
+    // Create a new node
+    new_node = malloc(sizeof(shash_node_t));
+    if (!new_node)
+        return 0;
+    new_node->key = strdup(key);
+    new_node->value = strdup(value);
+    if (!new_node->key || !new_node->value) {
+        free(new_node);
+        return 0;
+    }
+
+    // Insert node into the hash table
+    index = key_index((const unsigned char *)key, ht->size);
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
+
+    // Insert node into the sorted linked list
+    if (!ht->shead || strcmp(key, ht->shead->key) <= 0) {
+        new_node->sprev = NULL;
+        new_node->snext = ht->shead;
+        if (ht->shead)
+            ht->shead->sprev = new_node;
+        ht->shead = new_node;
+        if (!ht->stail)
+            ht->stail = new_node;
+    } else {
+        temp = ht->shead;
+        while (temp->snext && strcmp(key, temp->snext->key) > 0)
+            temp = temp->snext;
+        new_node->snext = temp->snext;
+        new_node->sprev = temp;
+        if (temp->snext)
+            temp->snext->sprev = new_node;
+        temp->snext = new_node;
+        if (!new_node->snext)
+            ht->stail = new_node;
+    }
+
+    return 1;
 }
 
 char *shash_table_get(const shash_table_t *ht, const char *key) {
@@ -44,23 +84,6 @@ char *shash_table_get(const shash_table_t *ht, const char *key) {
     }
 
     return NULL;
-}
-
-void shash_table_print(const shash_table_t *ht) {
-    shash_node_t *node;
-    int comma = 0;
-
-    if (!ht)
-        return;
-
-    printf("{");
-    for (node = ht->shead; node != NULL; node = node->snext) {
-        if (comma)
-            printf(", ");
-        printf("'%s': '%s'", node->key, node->value);
-        comma = 1;
-    }
-    printf("}\n");
 }
 
 void shash_table_print_rev(const shash_table_t *ht) {
