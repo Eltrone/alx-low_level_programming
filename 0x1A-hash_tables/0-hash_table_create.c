4e5d842d256596144f3cd0ae02f0eb65a93bdefa
Eltrone <amineelorche@gmail.com>
2023-12-22 09:51:04 -0800

    file added

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,33 +2,33 @@
 #include <stdlib.h>
 
 /**
- * hash_table_create - Crée une nouvelle table de hachage
- * @size: La taille du tableau de la table de hachage
+ * hash_table_create - Creates a new hash table
+ * @size: The size of the array of the hash table
  *
- * Return: Un pointeur vers la nouvelle table de hachage, ou NULL en cas d'échec
+ * Return: A pointer to the new hash table, or NULL on failure
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table;
-    unsigned long int i;
+	hash_table_t *hash_table;
+	unsigned long int i;
 
-    /* Allocation de mémoire pour la structure de la table de hachage */
-    hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL)
-        return (NULL);
+	/* Allocate memory for the hash table structure */
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
 
-    /* Initialisation de la table */
-    hash_table->size = size;
-    hash_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (hash_table->array == NULL)
-    {
-        free(hash_table);
-        return (NULL);
-    }
+	/* Initialize the table */
+	hash_table->size = size;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
 
-    /* Initialisation des pointeurs de nœuds à NULL */
-    for (i = 0; i < size; i++)
-        hash_table->array[i] = NULL;
+	/* Initialize node pointers to NULL */
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
 
-    return (hash_table);
+	return (hash_table);
 }
