42c12c17e23e3af549e1935a4244d601002ba23d
Eltrone <amineelorche@gmail.com>
2024-01-04 14:49:11 -0800

    file added

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Module pour définir la classe Rectangle."""
+
+
+class Rectangle:
+    """Classe qui définit un rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialisation de la classe Rectangle."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter pour width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter pour width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter pour height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter pour height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Retourne l'aire du rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Retourne le périmètre du rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Représentation en chaîne de caractères du rectangle avec le caractère #."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rectangle_str = []
+        for i in range(self.__height):
+            rectangle_str.append("#" * self.__width)
+        return "\n".join(rectangle_str)
+
+    def __repr__(self):
+        """Retourne une représentation de l'objet pour pouvoir recréer un nouveau rectangle."""
+        return f"Rectangle({self.__width}, {self.__height})"
