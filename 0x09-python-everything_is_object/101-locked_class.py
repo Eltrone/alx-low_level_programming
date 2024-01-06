68a2a956c1105b77038e0e41800efed53e4b6508
Eltrone <amineelorche@gmail.com>
2024-01-06 03:51:35 -0800

    file added

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 class LockedClass:
+    """
+    Cette classe ne permet que la création d'un attribut 'first_name'.
+    Toute tentative de création d'autres attributs générera une AttributeError.
+    """
     __slots__ = ['first_name']
