8cb83bfd7ad3d8ab916a817a1637158e0db7872a
Eltrone <amineelorche@gmail.com>
2024-01-07 15:12:49 -0800

    file added

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,36 @@
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :.
+
+    Args:
+    text (str): The text to be printed.
+
+    Raises:
+    TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text):
+        if text[i] in ".?:":
+            print(text[i])
+            print()
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        print(text[i], end="")
+        i += 1
+
+
+if __name__ == "__main__":
+    text_indentation("Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres")
---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
+
+    def test_single_element(self):
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_positive_numbers(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_negative_numbers(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_mixed_numbers(self):
+        self.assertEqual(max_integer([-1, 2, -3, 4]), 4)
+
+    def test_duplicate_numbers(self):
+        self.assertEqual(max_integer([4, 4, 4, 4]), 4)
+
+    def test_large_list(self):
+        self.assertEqual(max_integer(list(range(1, 10001))), 10000)
+
+if __name__ == '__main__':
+    unittest.main()
