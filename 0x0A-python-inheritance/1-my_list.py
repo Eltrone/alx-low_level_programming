a5c8ef93274bc7c3783db33a019345fb8a84729e
Eltrone <amineelorche@gmail.com>
2024-01-08 09:23:56 -0800

    file added

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,29 +1,9 @@
 #!/usr/bin/python3
-"""
-Module 1-my_list.
-Contient une classe MyList qui hérite de la classe list.
-"""
-
+""" Module pour la classe MyList qui hérite de list. """
 
 class MyList(list):
-    """
-    Classe MyList qui hérite de la classe list.
-    """
+    """ Classe MyList qui hérite de la classe intégrée list. """
 
     def print_sorted(self):
-        """
-        Imprime la liste, mais triée (tri ascendant).
-        """
+        """ Imprime la liste dans un ordre trié. """
         print(sorted(self))
-
-
-if __name__ == "__main__":
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
