98fc37279ecf3cba64628cfce1ae84c3bbce1f55
Eltrone <amineelorche@gmail.com>
2024-01-08 09:50:12 -0800

    file added

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-""" Module pour la classe Square qui hérite de Rectangle. """
+""" Classe Square héritant de Rectangle. """
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ Classe Square héritant de Rectangle. """
+    """ Classe Square qui hérite de la classe Rectangle. """
 
     def __init__(self, size):
         """
         Initialise un nouveau carré.
 
         Args:
-            size (int): La taille des côtés du carréentier positif.
+            size (int): La taille des côtés du carré entier positif.
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """ Retourne la description du carré. """
+        return "[Square] {}/{}".format(self.__size, self.__size)
