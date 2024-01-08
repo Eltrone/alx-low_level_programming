4192d94a7647d77e2a7aa0afe51f2e2c30d3e5e8
Eltrone <amineelorche@gmail.com>
2024-01-08 09:27:19 -0800

    file added

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Module pour la fonction is_same_class. """
 
+
 def is_same_class(obj, a_class):
     """
     Vérifie si un objet est une instance exacte d'une classe spécifiée.
