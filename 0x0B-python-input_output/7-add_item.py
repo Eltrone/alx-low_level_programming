f565f02ff9f6e337f0077815ab5a5ee605122af1
Eltrone <amineelorche@gmail.com>
2024-01-09 14:26:58 -0800

    file added

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
+""" Ajoute tous les arguments à une liste Python, puis enregistre cette liste dans un fichier """
 import sys
 
-# Assurez-vous d'avoir les fonctions load_from_json_file 
-# et save_to_json_file définies dans les fichiers respectifs
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+if __name__ == "__main__":
+    # Importation des fonctions depuis les fichiers 5-save_to_json_file et 6-load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file_name = "add_item.json"
+    try:
+        # Chargement de la liste existante depuis le fichier
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        # Si le fichier n'existe pas, initialiser une liste vide
+        items = []
 
-try:
-    items = load_from_json_file(file_name)
-except (FileNotFoundError, TypeError):
-    items = []
+    # Ajout des arguments de la ligne de commande à la liste
+    items.extend(sys.argv[1:])
 
-# Ajouter les arguments de la ligne de commande à la liste 
-#en excluant le nom du script
-items.extend(sys.argv[1:])
-
-# Enregistrer la liste mise à jour dans le fichier JSON
-save_to_json_file(items, file_name)
-
+    # Enregistrement de la liste mise à jour dans le fichier add_item.json
+    save_to_json_file(items, "add_item.json")
