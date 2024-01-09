2591fdbb31bc8005405451e2f2bfb35c5523c4f6
Eltrone <amineelorche@gmail.com>
2024-01-09 14:17:54 -0800

    file added

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,35 +6,32 @@ class Student:
     """Classe représentant un étudiant."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialise une nouvelle instance de la classe Student.
-
-        Args:
-            first_name (str): Le prénom de l'étudiant.
-            last_name (str): Le nom de famille de l'étudiant.
-            age (int): L'âge de l'étudiant.
-        """
+        """Initialise une nouvelle instance de Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Renvoie une représentation dictionnaire de l'instance Student.
+        """Renvoie une représentation dictionnaire de l'instance.
 
         Args:
-            attrs (list): Liste des attributs à inclure dans le dictionnaire.
+            attrs (list): Attributs à inclure dans le dictionnaire.
 
         Returns:
-            dict: Un dictionnaire contenant les attributs spécifiés de l'étudiant.
+            dict: Dictionnaire des attributs spécifiés.
         """
         if attrs is not None and all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs if hasattr(self, attr)
+            }
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Met à jour les attributs de l'instance à partir d'un dictionnaire.
+        """Met à jour les attributs à partir d'un dictionnaire.
 
         Args:
-            json (dict): Un dictionnaire contenant les nouveaux attributs.
+            json (dict): Dictionnaire des nouveaux attributs.
         """
         for key, value in json.items():
             if hasattr(self, key):
