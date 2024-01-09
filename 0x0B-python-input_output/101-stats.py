59f977cff03a8dbec3c01aeb34ca90e1ce397afe
Eltrone <amineelorche@gmail.com>
2024-01-09 14:40:42 -0800

    file added

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,45 +1,40 @@
+#!/usr/bin/python3
 import sys
 import signal
+
+def print_stats(total_size, status_codes):
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        print("{}: {}".format(code, status_codes[code]))
+
+def signal_handler(sig, frame):
+    print_stats(total_size, status_codes)
+    sys.exit(0)
 
 # Initialisation des variables
 total_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 
-def print_stats():
-    """ Fonction pour imprimer les statistiques """
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
-
-def signal_handler(sig, frame):
-    """ Gère l'interruption du clavier (CTRL + C) """
-    print_stats()
-    sys.exit(0)
-
-# Configuration du gestionnaire de signal pour CTRL + C
+# Configuration du gestionnaire de signal pour CTRL+C
 signal.signal(signal.SIGINT, signal_handler)
 
-# Lecture de stdin ligne par ligne
 try:
     for line in sys.stdin:
         parts = line.split()
-        try:
-            size = int(parts[-1])
-            status_code = int(parts[-2])
-            total_size += size
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except (ValueError, IndexError):
-            pass  # Gère les erreurs de format de ligne
+        status_code = int(parts[-2])
+        file_size = int(parts[-1])
+
+        # Mise à jour des statistiques
+        total_size += file_size
+        if status_code in status_codes:
+            status_codes[status_code] += 1
 
         line_count += 1
         if line_count % 10 == 0:
-            print_stats()
+            print_stats(total_size, status_codes)
 
 except KeyboardInterrupt:
     pass
-
-# Affiche les statistiques finales
-print_stats()
+finally:
+    print_stats(total_size, status_codes)
