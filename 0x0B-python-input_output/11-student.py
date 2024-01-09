39cd6addebd9bcbb17de0e96dfb10f90182ced90
Eltrone <amineelorche@gmail.com>
2024-01-09 09:30:51 -0800

    file added

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module définissant la classe Student avec filtration des attributs."""
+"""Module définissant la classe Student avec mise à jour depuis un JSON."""
 
 
 class Student:
@@ -21,11 +21,21 @@ class Student:
         """Renvoie une représentation dictionnaire de l'instance Student.
 
         Args:
-            attrs (list): Liste attributs à inclure dans le dictionnaire.
+            attrs (list): Liste des attributs à inclure dans le dictionnaire.
 
         Returns:
-            dict: Un dictionnaire avec attributs spécifiés de l'étudiant.
+            dict: Un dictionnaire contenant les attributs spécifiés de l'étudiant.
         """
         if attrs is not None and all(isinstance(attr, str) for attr in attrs):
             return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Met à jour les attributs de l'instance à partir d'un dictionnaire.
+
+        Args:
+            json (dict): Un dictionnaire contenant les nouveaux attributs.
+        """
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
