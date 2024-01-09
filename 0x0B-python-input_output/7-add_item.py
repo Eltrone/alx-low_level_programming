02d8173e23cc046f682d6fae0754842a140f05f1
Eltrone <amineelorche@gmail.com>
2024-01-09 14:11:34 -0800

    file added

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,10 +3,11 @@
 
 import sys
 
-
 if __name__ == "__main__":
+    # Importation divisée en deux lignes pour réduire la longueur
+    load_module = __import__('6-load_from_json_file')
+    load_from_json_file = load_module.load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     filename = "add_item.json"
 
