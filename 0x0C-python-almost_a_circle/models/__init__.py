46c0b52f25911718503501cdf3b9727ed7c6a3ce
EL ORCHE <amineelorche@gmail.com>
2024-01-15 09:50:20 -0800

    file added

---
+++ 0x0C-python-almost_a_circle/models/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module base.py - Defines a base class for all models in the project."""
+
+class Base:
+    """Base class for all other classes in the project."""
+    __nb_objects = 0  # Private class attribute
+
+    def __init__(self, id=None):
+        """Initializes the Base instance.
+
+        Args:
+            id (int): The id of the instance. If None, increment __nb_objects.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
