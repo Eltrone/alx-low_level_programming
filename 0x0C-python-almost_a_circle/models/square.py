806bc59b2c38f6fdd1fe1f6785643b3ac5e498f4
EL ORCHE <amineelorche@gmail.com>
2024-01-15 15:15:52 -0800

    file added

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Module square.py - Defines Square class, inheriting from Rectangle. """
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ 
+    Square class that inherits from Rectangle.
+
+    Attributes are inherited from Rectangle.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square's sides.
+            x (int): The x coordinate of the square.
+            y (int): The y coordinate of the square.
+            id (int): The id of the square.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return a string representation of the Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
