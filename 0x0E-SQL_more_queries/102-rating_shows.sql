7f5dac610ca54f41be114b45fc033abdbc2565df
EL ORCHE <amineelorche@gmail.com>
2024-02-15 08:47:57 -0800

    file added

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- List all shows from hbtn_0d_tvshows_rate by their rating
-SELECT tv_shows.title, SUM(rating) AS rating
-FROM tv_shows
-JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title
+SELECT s.title, SUM(r.rate) AS rating
+FROM tv_shows s
+JOIN tv_show_ratings r ON s.id = r.show_id
+GROUP BY s.title
 ORDER BY rating DESC;
