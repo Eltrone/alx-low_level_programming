dd25d1c8d5ab6b206b90d7ef9d5a5d8f588c9821
EL ORCHE <amineelorche@gmail.com>
2024-02-15 08:46:41 -0800

    file added

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
--- List all genres in hbtn_0d_tvshows_rate by their rating
-SELECT tv_genres.name, SUM(tv_show_ratings.rating) AS rating
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
-GROUP BY tv_genres.name
+-- List all genres in the database hbtn_0d_tvshows_rate by their rating
+SELECT g.name AS name, SUM(r.rate) AS rating
+FROM tv_genres g
+JOIN tv_show_genres sg ON g.id = sg.genre_id
+JOIN tv_show_ratings r ON sg.show_id = r.show_id
+GROUP BY g.name
 ORDER BY rating DESC;
