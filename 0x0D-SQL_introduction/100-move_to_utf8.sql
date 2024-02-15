662d995a328513f7b2b837b3ca840dec22edda5c
EL ORCHE <amineelorche@gmail.com>
2024-02-15 09:14:56 -0800

    file added

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,15 @@
--- Convert hbtn_0c_0 database and first_table to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convertir la base de données en utf8mb4
+ALTER DATABASE `hbtn_0c_0` 
+CHARACTER SET = utf8mb4 
+COLLATE = utf8mb4_unicode_ci;
+
+-- Convertir la table en utf8mb4
+ALTER TABLE `first_table` 
+CONVERT TO CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
+
+-- Convertir le champ spécifique en utf8mb4
+ALTER TABLE `first_table` 
+CHANGE `name` `name` VARCHAR(256) 
+CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
