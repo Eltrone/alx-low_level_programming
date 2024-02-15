f5f9a22f3b759e96532a274aedac5ccfc554273a
EL ORCHE <amineelorche@gmail.com>
2024-02-15 09:13:19 -0800

    file added

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,5 @@
--- Display the average temperature by city ordered by temperature descendingI
-SELECT city, AVG(temperature) AS avg_temp
+-- Display the average temperature by city ordered by temperature descending
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE MONTH(recorded_date) IN (7, 8)
 GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+ORDER BY avg_temp DESC;
