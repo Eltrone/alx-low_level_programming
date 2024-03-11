65cc720d194b321c4edcec7a0954ecf02ac8558e
Eltrone <amineelorche@gmail.com>
2024-03-11 15:07:08 -0700

    file added

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-function callMeMoby(times, theFunction) {
-  for (let i = 0; i < times; i++) {
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
     theFunction();
   }
-}
-
-callMeMoby(3, () => console.log('C is fun'));
+};
