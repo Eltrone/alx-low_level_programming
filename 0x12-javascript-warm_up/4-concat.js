de96fd9bad4b9d097998165777c646631ffe8346
Eltrone <amineelorche@gmail.com>
2024-03-11 09:25:02 -0700

    file added

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+if (firstArg && secondArg) {
+  console.log(firstArg, 'is', secondArg);
+} else {
+  console.log(firstArg || 'undefined', 'is', secondArg || 'undefined');
+}
