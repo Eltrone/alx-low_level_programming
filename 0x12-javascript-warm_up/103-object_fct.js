5500414622b776c0577f6d72025eed6e819135c3
Eltrone <amineelorche@gmail.com>
2024-03-11 15:08:52 -0700

    file added

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-
 const myObject = {
-  type: "object",
-  value: 12,
-  incr() {
-    this.valeur++;
-  },
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value += 1;
 };
 
 myObject.incr();
