db3e48b170f06fb21c1d15fa5baba66bc0366911
Eltrone <amineelorche@gmail.com>
2024-03-22 18:51:23 -0700

    file added

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-"""
-List all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa
-"""
+"""List all State objects, and corresponding City objects from the database"""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State  # Assurez-vous que ces modules sont correctement définis
+from relationship_state import Base, State
+from relationship_city import City
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    # Créer la connexion à la base de données
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
+    # Create the connection to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    # Créer une session
+    # Create the session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Requête pour récupérer tous les objets State avec leurs villes correspondantes
-    # en utilisant la relation 'cities' définie dans le modèle State
-    states = session.query(State).order_by(State.id, State.cities.any().id).all()
+    # One query to the database using the cities relationship for all State objects
+    states = session.query(State).order_by(State.id, City.id).all()
 
+    # Display results sorted by states.id and cities.id
     for state in states:
-        print(f"{state.id}: {state.name}")
+        print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print(f"\t{city.id}: {city.name}")
-
-    session.close()
+            print("\t{}: {}".format(city.id, city.name))
