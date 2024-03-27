34e500b6c15598f92f7c0519daf84f0c4283f6af
Eltrone <amineelorche@gmail.com>
2024-03-27 10:05:01 -0700

    file added

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Envoie requête POST avec le contenu d'un fichier JSON à une URL et affiche le corps de la réponse
-curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
+curl -s -H POST -H "Content-Type: application/json" -d @"$2" "$1"
 
