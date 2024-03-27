7d0f603ea59f80f4208e04345f8941f6ca3b8d00
Eltrone <amineelorche@gmail.com>
2024-03-27 09:19:48 -0700

    file added

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# envoie une requête GET à une URL et affiche uniquement le corps de la réponse pour un code de statut 200
+# envoie une requête GET à une URL et affiche uniquement le corps de la réponse
 curl -sf "$1"
