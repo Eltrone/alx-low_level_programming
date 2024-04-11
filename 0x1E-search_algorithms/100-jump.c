2f5f23006ecbbde9d09f805c686d7612bed5162a
Eltrone <amineelorche@gmail.com>
2024-04-11 10:11:20 -0700

    file added

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,40 +12,38 @@
  * Return: The first index where value is located,
  * or -1 if value is not present in array or if array is NULL
  */
-
 int jump_search(int *array, size_t size, int value)
 {
-    size_t jump = sqrt(size);
-    size_t prev = 0;
-    size_t left, right, i;
+	size_t jump = sqrt(size);
+	size_t prev = 0;
+	size_t left, right, i;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    while (prev < size && array[prev] < value)
-    {
-        printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
-        prev += jump;
-    }
+	while (prev < size && array[prev] < value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
+		prev += jump;
+	}
 
-    left = prev < jump ? 0 : prev - jump;
- 
-    
-    right = (prev < size) ? prev : prev - 1;
+	left = prev < jump ? 0 : prev - jump;
+	right = (prev < size) ? prev : prev - 1;
 
-    
-    
-    printf("Value found between indexes [%ld] and [%ld]\n", left, (prev < size) ? right : prev);
+	printf("Value found between indexes [%ld] and [%ld]\n", left,
+		   (prev < size) ? right : prev);
 
-    right = min(size - 1, right);
-    for (i = left; i <= right; ++i)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (int)i;
-    }
+	right = min(size - 1, right);
+	for (i = left; i <= right && i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return ((int)i);
+		}
+	}
 
-    return (-1);
+	return (-1);
 }
 
 /**
@@ -56,5 +54,5 @@ int jump_search(int *array, size_t size, int value)
  */
 size_t min(size_t a, size_t b)
 {
-    return (a < b) ? a : b;
+	return ((a < b) ? a : b);
 }
